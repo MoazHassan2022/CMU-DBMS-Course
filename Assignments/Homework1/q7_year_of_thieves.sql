5e20386990c668df6be0aa824dc6cbb3f6d4d49f
MoazHassan2022 <87096647+MoazHassan2022@users.noreply.github.com>
2023-03-09 00:56:13 +0200

    edited question 7 years of thieves

--- Assignments/Homework1/q7_year_of_thieves.sql
+++ Assignments/Homework1/q7_year_of_thieves.sql
@@ -2,7 +2,7 @@ WITH certain_premiered_work AS (
     SELECT
         premiered
     FROM titles
-    WHERE primary_title = 'Friends'
+    WHERE primary_title = 'Army of Thieves'
 )
 SELECT COUNT(DISTINCT t.title_id)
 FROM titles as t, certain_premiered_work as c
